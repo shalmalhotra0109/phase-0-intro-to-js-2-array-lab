1a72d716c7c9f35331abeab86509ed3db97b82c9
Shal Malhotra <smalhotra0109@gmail.com>
2023-09-25 16:55:24 -0400

    changes to cats index

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return ["Arnold", ...cats];
+  }
+   function removeLastCat() {
+   let newcats = [...cats] 
+    newcats.pop();
+    return newcats;
+  }
+  function removeFirstCat() {
+    let newcats = [...cats]
+    newcats.shift();
+    return newcats;
+  }
+
+  
+
